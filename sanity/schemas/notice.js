4818616218814eebb7a1113e3af3dec6f4fee81e
Davide Brognoli <davide.brognoli@gmail.com>
2020-10-31 15:22:59 +0100

    Change notice description type

--- sanity/schemas/notice.js
+++ sanity/schemas/notice.js
@@ -11,7 +11,12 @@ export default {
       {
         title: 'Descrizione',
         name: 'description',
-        type: 'text'
+        type: 'array',
+        of: [
+          {
+            type: 'block'
+          }
+        ]
       },
       {
         title: 'Data',
