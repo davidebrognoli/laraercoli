1e96db830a685f1381b4db666cc9dcd2e06df99c
Davide Brognoli <davide.brognoli@codiceplastico.com>
2025-09-02 21:25:48 +0200

    Add normalize.css for consistent styling across browsers; refactor SCSS imports to use @use syntax

--- gatsby/gatsby-browser.js
+++ gatsby/gatsby-browser.js
@@ -4,4 +4,5 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+// Import normalize.css for modern browser reset
+import 'normalize.css'
